ce416f5ef363ad5c88733860ddb53038eb01f5d2
Timothy Sabat <timothy.sabat@gmail.com>
2014-07-07 10:26:43 -0700

    adding rails_config and modifying secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :pty, true
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ set :linked_dirs, %w{conf bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
