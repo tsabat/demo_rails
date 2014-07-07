39a22193db0eef299c624d2ced6485d656961e6e
Timothy Sabat <timothy.sabat@gmail.com>
2014-07-07 11:47:43 -0700

    remove the settings import

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -1,6 +1,6 @@
 RailsConfig.setup do |config|
   config.const_name = "Settings"
-  if Rails.env.production?
-    Settings.add_source!("#{Rails.root}/config/settings/secret.yml")
-  end
+  #if Rails.env.production?
+    #Settings.add_source!("#{Rails.root}/config/settings/secret.yml")
+  #end
 end
